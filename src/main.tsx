95c9b55602ad87c513b0a4fdb1ce3e531fd6e8b5
Puneet56 <puneetdhiman56@gmail.com>
2025-03-28 21:25:29 +0530

    Added posthog

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { PostHogProvider } from "posthog-js/react";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -13,13 +14,22 @@ if (!savedTheme || savedTheme === "dark") {
 
 const queryClient = new QueryClient();
 
+const options = {
+	api_host: import.meta.env.VITE_APP_PUBLIC_POSTHOG_HOST,
+};
+
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<QueryClientProvider client={queryClient}>
-				<App />
-			</QueryClientProvider>
-		</BrowserRouter>
+		<PostHogProvider
+			apiKey={import.meta.env.VITE_APP_PUBLIC_POSTHOG_KEY}
+			options={options}
+		>
+			<BrowserRouter>
+				<QueryClientProvider client={queryClient}>
+					<App />
+				</QueryClientProvider>
+			</BrowserRouter>
+		</PostHogProvider>
 	</StrictMode>,
 );
